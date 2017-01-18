6cd10a161f07a3e94af9d9329f0b42fe42bd0378
Yusuke Kuoka <ykuoka@gmail.com>
2017-01-18 15:46:44 +0900

    fix: Spot Fleet doesn't support the t2 instance family
    Defaults to the c4 family rather than t2 for spot fleets

--- model/worker.go
+++ model/worker.go
@@ -51,9 +51,8 @@ func newDefaultSpotFleet() SpotFleet {
 		UnitRootVolumeSize: 30,
 		RootVolumeType:     "gp2",
 		LaunchSpecifications: []LaunchSpecification{
-			NewLaunchSpecification(1, "t2.medium"),
-			NewLaunchSpecification(2, "m3.large"),
-			NewLaunchSpecification(2, "m4.large"),
+			NewLaunchSpecification(1, "c4.large"),
+			NewLaunchSpecification(2, "c4.xlarge"),
 		},
 	}
 }
