01bc8dbe4baafe4dd2102191d0f5d2d8aa67e8b3
Yusuke Kuoka <ykuoka@gmail.com>
2017-03-22 09:41:23 +0900

    Repair CF rollbacks to work after the S3 userdata improvement Fixes #352

--- core/nodepool/config/stack_config.go
+++ core/nodepool/config/stack_config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/coreos/kube-aws/coreos/userdatavalidation"
 	"github.com/coreos/kube-aws/filereader/jsontemplate"
+	"github.com/coreos/kube-aws/fingerprint"
 	"net/url"
 )
 
@@ -17,12 +18,37 @@ type CompressedStackConfig struct {
 	*StackConfig
 }
 
-func (c *StackConfig) UserDataWorkerS3Path() (string, error) {
+// UserDataWorkerS3Prefix is the prefix prepended to all user-data-worker-<fingerprint> files uploaded to S3
+// Use this to author the IAM policy to provide worker nodes least required permissions for getting the files from S3
+func (c *StackConfig) UserDataWorkerS3Prefix() (string, error) {
+	s3dir, err := c.userDataWorkerS3Directory()
+	if err != nil {
+		return "", fmt.Errorf("Error in UserDataWorkerS3Prefix : %v", err)
+	}
+	return fmt.Sprintf("%s/userdata-worker", s3dir), nil
+}
+
+func (c *StackConfig) userDataWorkerS3Directory() (string, error) {
 	s3uri, err := url.Parse(c.S3URI)
 	if err != nil {
-		return "", fmt.Errorf("Error in UserDataWorkerS3Path : %v", err)
+		return "", fmt.Errorf("Error in userDataWorkerS3Directory : %v", err)
 	}
-	return fmt.Sprintf("%s%s/%s/userdata-worker", s3uri.Host, s3uri.Path, c.StackName()), nil
+	return fmt.Sprintf("%s%s/%s", s3uri.Host, s3uri.Path, c.StackName()), nil
+}
+
+// UserDataWorkerS3URI is the URI to an userdata-worker-<fingerprint> file used to provision worker nodes
+// Use this to run download the file by running e.g. `aws cp *return value of UserDataWorkerS3URI* ./`
+func (c *StackConfig) UserDataWorkerS3URI() (string, error) {
+	s3dir, err := c.userDataWorkerS3Directory()
+	if err != nil {
+		return "", fmt.Errorf("Error in UserDataWorkerS3URI : %v", err)
+	}
+	return fmt.Sprintf("s3://%s/%s", s3dir, c.UserDataWorkerFileName()), nil
+}
+
+// UserDataWorkerFileName is used to upload and download userdata-worker-<fingerprint> files
+func (c *StackConfig) UserDataWorkerFileName() string {
+	return "userdata-worker-" + fingerprint.SHA256(c.UserDataWorker)
 }
 
 func (c *StackConfig) ValidateUserData() error {
