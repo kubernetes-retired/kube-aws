9c01a23281e893703601d264c30d3ad4811d65ef
Yusuke Kuoka <ykuoka@gmail.com>
2017-03-22 09:41:23 +0900

    Repair CF rollbacks to work after the S3 userdata improvement Fixes #352

---
+++ fingerprint/sha256.go
@@ -0,0 +1,13 @@
+package fingerprint
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+// SHA256 calculates and returns a SHA-256 hash intended to be used for a fingerprint of the original data
+func SHA256(data string) string {
+	h := sha256.New()
+	h.Write([]byte(data))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
