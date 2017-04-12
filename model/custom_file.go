7b53261f165f01ade50216765d3571df9a6282dd
Jeremy Deininger <jeremydeininger@gmail.com>
2017-04-12 10:06:46 -0700

    use gzip base64 encoding for customFiles

--- model/custom_file.go
+++ model/custom_file.go
@@ -2,7 +2,8 @@ package model
 
 import (
 	"fmt"
-	"strings"
+
+	"github.com/kubernetes-incubator/kube-aws/gzipcompressor"
 )
 
 type CustomFile struct {
@@ -12,12 +13,15 @@ type CustomFile struct {
 	UnknownKeys `yaml:",inline"`
 }
 
-func (c CustomFile) ContentArray() []string {
-	// CustomFiles needs an array so that we can inline it.
-	return strings.Split(c.Content, "\n")
-}
-
 func (c CustomFile) PermissionsString() string {
 	// We also need to write out octal notation for permissions.
 	return fmt.Sprintf("0%o", c.Permissions)
+}
+
+func (c CustomFile) GzippedBase64Content() string {
+	out, err := gzipcompressor.CompressString(c.Content)
+	if err != nil {
+		return ""
+	}
+	return out
 }
