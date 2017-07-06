9e8eaca7cfb9ca9a43ae51df25dce52c5ee537cb
Joseph Hollinshead <jhollinshead@hotels.com>
2017-07-06 10:23:12 +0100

    Include 'omitempty' for SystemdMessageResponse struct variables

--- model/cloudwatch_logging.go
+++ model/cloudwatch_logging.go
@@ -1,12 +1,12 @@
 package model
 
 type SystemdMessageResponse struct {
-	InstanceId  string `json:"instanceId"`
-	Hostname    string `json:"hostname"`
-	CmdName     string `json:"cmdName"`
-	Exe         string `json:"exe"`
-	CmdLine     string `json:"cmdLine"`
-	SystemdUnit string `json:"systemdUnit"`
-	Priority    string `json:"priority"`
-	Message     string `json:"message"`
+	InstanceId  string `json:"instanceId,omitempty"`
+	Hostname    string `json:"hostname,omitempty"`
+	CmdName     string `json:"cmdName,omitempty"`
+	Exe         string `json:"exe,omitempty"`
+	CmdLine     string `json:"cmdLine,omitempty"`
+	SystemdUnit string `json:"systemdUnit,omitempty"`
+	Priority    string `json:"priority,omitempty"`
+	Message     string `json:"message,omitempty"`
 }
