57f420c04d2b8e89a1ef46db4359f12d2ef2fb0d
Camil Blanaru <camil@me.com>
2017-11-24 03:58:19 +0100

    Metrics Server addon. (#973)
    More info here:
    https://kubernetes.io/docs/tasks/debug-application-cluster/core-metrics-
    pipeline/

--- core/controlplane/config/config.go
+++ core/controlplane/config/config.go
@@ -152,6 +152,7 @@ func NewDefaultCluster() *Cluster {
 			HelmImage:                          model.Image{Repo: "quay.io/kube-aws/helm", Tag: "v2.6.0", RktPullDocker: false},
 			TillerImage:                        model.Image{Repo: "gcr.io/kubernetes-helm/tiller", Tag: "v2.6.2", RktPullDocker: false},
 			HeapsterImage:                      model.Image{Repo: "gcr.io/google_containers/heapster", Tag: "v1.4.3", RktPullDocker: false},
+			MetricsServerImage:                 model.Image{Repo: "gcr.io/google_containers/metrics-server-amd64", Tag: "v0.2.0", RktPullDocker: false},
 			AddonResizerImage:                  model.Image{Repo: "gcr.io/google_containers/addon-resizer", Tag: "2.1", RktPullDocker: false},
 			KubeDashboardImage:                 model.Image{Repo: "gcr.io/google_containers/kubernetes-dashboard-amd64", Tag: "v1.7.1", RktPullDocker: false},
 			KubeDashboardInitImage:             model.Image{Repo: "gcr.io/google_containers/kubernetes-dashboard-init-amd64", Tag: "v1.0.1", RktPullDocker: false},
@@ -440,6 +441,7 @@ type DeploymentSettings struct {
 	HelmImage                          model.Image `yaml:"helmImage,omitempty"`
 	TillerImage                        model.Image `yaml:"tillerImage,omitempty"`
 	HeapsterImage                      model.Image `yaml:"heapsterImage,omitempty"`
+	MetricsServerImage                 model.Image `yaml:"metricsServerImage,omitempty"`
 	AddonResizerImage                  model.Image `yaml:"addonResizerImage,omitempty"`
 	KubeDashboardImage                 model.Image `yaml:"kubeDashboardImage,omitempty"`
 	KubeDashboardInitImage             model.Image `yaml:"kubeDashboardInitImage,omitempty"`
