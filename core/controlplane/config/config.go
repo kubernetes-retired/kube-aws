4e713f6c471a3ed7bb269931dfb0457664d34434
Chris Knowles <c-knowles@users.noreply.github.com>
2018-07-09 08:37:18 +0800

    Self hosted calico v3.1.3 (#1397)
    - Bump calico images for self hosted Calico
    - Include latest CRDs and RBAC for canal from https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/flannel
    - Correct the self hosted flannel images to self hosted config (previously unused)
    - Mount calico dirs in canal and kubelet to fix pod init issue, ref https://github.com/projectcalico/calico/issues/1795#issuecomment-390171235
    
    Fixes https://github.com/kubernetes-incubator/kube-aws/issues/1396
    
    Changelog:
    
    * Self hosted calico v3.1.3
    
    - Bump calico images for self hosted Calico (non-self hosted is EOL)
    - Include latest CRDs and RBAC for canal from https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/flannel
    
    * Link self hosted flannel images to self hosted config
    
    Previously the older flannel config was used and the self hosted config in cluster.yaml did nothing
    
    * Update canal comment to a reference
    
    Our config is dynamic so would change anyway if a user configures the versions differently
    
    * Mount calico dirs in canal and kubelet
    
    Fixes pod init issue similar to https://github.com/projectcalico/calico/issues/1795#issuecomment-390171235:
    ```
    Failed create pod sandbox: rpc error: code = Unknown desc = NetworkPlugin cni failed to set up pod "xxxx" network: stat /var/lib/calico/nodename: no such file or directory: check that the calico/node container is running and has mounted /var/lib/calico/
    ```

--- core/controlplane/config/config.go
+++ core/controlplane/config/config.go
@@ -35,11 +35,11 @@ const (
 	userDataDir    = "userdata"
 
 	// Experimental SelfHosting feature default images.
-	kubeNetworkingSelfHostingDefaultCalicoNodeImageTag = "v3.0.6"
-	kubeNetworkingSelfHostingDefaultCalicoCniImageTag  = "v2.0.5"
+	kubeNetworkingSelfHostingDefaultCalicoNodeImageTag = "v3.1.3"
+	kubeNetworkingSelfHostingDefaultCalicoCniImageTag  = "v3.1.3"
 	kubeNetworkingSelfHostingDefaultFlannelImageTag    = "v0.9.1"
 	kubeNetworkingSelfHostingDefaultFlannelCniImageTag = "v0.3.0"
-	kubeNetworkingSelfHostingDefaultTyphaImageTag      = "v0.6.4"
+	kubeNetworkingSelfHostingDefaultTyphaImageTag      = "v0.7.4"
 
 	// ControlPlaneStackName is the logical name of a CloudFormation stack resource in a root stack template
 	// This is not needed to be unique in an AWS account because the actual name of a nested stack is generated randomly
