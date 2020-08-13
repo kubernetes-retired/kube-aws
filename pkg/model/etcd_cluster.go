5720daec40ca7833e17a957dc5de7ddcc0ef4294
Dominic Gunn <dominic@fable.sh>
2020-08-13 14:58:33 +0100

    Fix tests

--- pkg/model/etcd_cluster.go
+++ pkg/model/etcd_cluster.go
@@ -45,7 +45,12 @@ func (c EtcdCluster) DNSNames() []string {
 			dnsName = fmt.Sprintf("*.%s", c.region.PrivateDomainName())
 		}
 	}
-	return []string{dnsName, fmt.Sprintf("*.%s", c.region.PrivateDomainName())}
+
+	privateDomainSan := fmt.Sprintf("*.%s", c.region.PrivateDomainName())
+	if dnsName != privateDomainSan && c.GetMemberIdentityProvider() == api.MemberIdentityProviderENI {
+		return []string{dnsName, privateDomainSan}
+	}
+	return []string{dnsName}
 }
 
 func (c EtcdCluster) LogicalName() string {
